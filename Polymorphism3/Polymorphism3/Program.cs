4e4d46068d44bc2631c943b75252a8e388654741
Ibrahim Khalafov <ibrahimjk@code.edu.az>
2026-09-02 02:43:02 +0400

    Student task added

---
+++ Polymorphism3/Polymorphism3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Polymorphism3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Abbas Quliyev", "A123", 20);
+
+            Console.WriteLine($"Full Name: {student.FullName}");
+            Console.WriteLine($"Group No: {student.GroupNo}");
+            Console.WriteLine($"Age: {student.Age}");
+        }
+    }
+}
---
+++ Polymorphism3/Polymorphism3/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism3
+{
+    internal class Student
+    {
+        public string FullName { get; set; }
+        public string GroupNo { get; set; }
+        public int Age { get; set; }
+
+        public Student(string fullname, string groupno, int age)
+        {
+             FullName = fullname;
+             GroupNo = groupno;
+             Age = age;
+        }
+        public Student(string groupno)
+        {
+            if (char.IsUpper(GroupNo[0]) && GroupNo.Length == 4 &&
+                char.IsDigit(GroupNo[1]) && char.IsDigit(GroupNo[2]) && char.IsDigit(GroupNo[3]))
+            {
+                GroupNo = groupno;
+            }
+        }     
+        public Student(string fullname, int age) 
+        {
+            string[] parts = fullname.Split(' ');
+
+            if (parts.Length == 2 )
+            {
+                FullName = fullname;
+            }
+            else
+            {
+                Console.WriteLine("Fullname duzgun deyil!");
+            }
+            Age = age;
+        }
+    }
+
+}
